699d76891349ca9f58130a7d7bd214cb2c459c4c
Rob Fitzgerald, Inc <rfitzgerald@c-vusd.org>
2018-03-17 04:59:12 -0700

    Created an email sign up and sign in with Firebase

--- AuthGoogle/ViewController.swift
+++ AuthGoogle/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signUp(_ sender: Any) {
+            Auth.auth().createUser(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+                if user != nil {
+                    print ("User Has Signed Up!")
+                }
+                if error != nil {
+                    print(":(")
+                }
+            }
+        
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+            if user != nil {
+                print ("User Has Signed In!")
+            }
+            if error != nil {
+                print(":(")
+            }
+        }
+        
+    }
+    
 }
 
