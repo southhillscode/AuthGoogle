0147de05c33b295f141ddb2f0a9002fd65d178ee
Rob Fitzgerald, Inc <rfitzgerald@c-vusd.org>
2018-03-17 04:12:36 -0700

    Initial Commit

---
+++ AuthGoogle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthGoogle
+//
+//  Created by Rob on 3/17/18.
+//  Copyright © 2018 Rob Fitzgerald, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
